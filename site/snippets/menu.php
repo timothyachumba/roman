ed47068e6be24604a5a62224c592f9e3ccb93e44
Timothy Achumba <tim@6wunderkinder.com>
2014-10-06 18:32:07 +0200

    The count yo

--- site/snippets/menu.php
+++ site/snippets/menu.php
@@ -1,10 +1,12 @@
 <nav class="menu">
   <ol class="rows">
+    <?php $count = 0; ?>
     <?php foreach($pages->find('projects')->children() as $project): ?>
+      <?php $count++; ?>
       <li>
         <div>
           <a href="#" class="<?= $project->uid() ?> project-link">
-            <span class="number">1</span>
+            <span class="number"><?= $count ?></span>
             <span class="name"><?= $project->title() ?></span>
           </a>
         </div>
