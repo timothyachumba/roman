c3d901b2d1fdd87621544eb70218bac6f23e458f
Timothy Achumba <tim@6wunderkinder.com>
2015-02-04 19:00:49 +0100

    Fixed mobile styles

--- site/accounts/admin.php
+++ site/accounts/admin.php
@@ -8,4 +8,4 @@ password: >
   $2a$10$2xcVjC1SNAfMTy2FLH0Z.eHYhLmElL8iwkJSqKOK4e.XVnp38lrOq
 language: en
 role: admin
-token: 9f18ae46042b6b7d45207ea427566f2145dcb65a
+token: ac923c6232acb80a61890e91d0b2afc0c344facc
